1156ccc55094f431616234d94499525e628bd62e
6ichem <hiclesbat@gmail.com>
2024-07-10 21:38:40 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img
+            src="https://www.gac.com/webassets/img/gac-logo.svg"
+            className="logo"
+            alt="Vite logo"
+          />
+        </a>
+      </div>
+      <h1>GAC Technical Challenge</h1>
+    </>
+  );
+}
+
+export default App;
